c5dfedad764c5f90ad6e1f8f1e6d5f070f7be518
Jose Apaza <joseapaza883@gmail.com>
2021-09-15 09:09:48 -0500

    Error arreglado de location

--- enviar.php
+++ enviar.php
@@ -4,7 +4,7 @@
     $contenido = "Email: " . $nombre;
 
     mail($destino,"Contacto",$contenido);
-    header("Location:index.html"); 
+    header("Location:https://www.figma.com/proto/HLa43RkdveNa5AsFz8HA5m/iQuela-prototipo?node-id=2%3A2&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=51%3A64"); 
 
 
 ?>
